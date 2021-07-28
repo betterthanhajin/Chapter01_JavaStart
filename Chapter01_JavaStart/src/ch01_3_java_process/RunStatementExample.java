a104bfaab670eb5f4c3216bd73cb8d6a3aaab4c4
bitcamp <bitcamp@DESKTOP-7NUJNL8>
2021-07-28 09:44:35 +0900

    ffff

---
+++ Chapter01_JavaStart/src/ch01_3_java_process/RunStatementExample.java
@@ -0,0 +1,22 @@
+package ch01_3_java_process;
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * [두 수의 합을 출력]
+		 * 두 수를 선언
+		 * 두 수의 합 연산
+		 * 연산 결과 저장
+		 * 저장된 값을 출력
+		 */
+		
+		int x,y;
+		x = 3;
+		y = 2;
+		int z = x + y;
+		System.out.println(z);
+		
+	}
+}
